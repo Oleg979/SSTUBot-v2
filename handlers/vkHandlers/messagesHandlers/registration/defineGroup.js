89cda12102d72410e9977bba6e181d86fd5b5272
Олег <olegsolovev506@gmail.com>
2018-08-19 14:06:21 +0400

    фвв

--- handlers/vkHandlers/messagesHandlers/registration/defineGroup.js
+++ handlers/vkHandlers/messagesHandlers/registration/defineGroup.js
@@ -2,21 +2,35 @@ var createKeyboard = require("../createKeyboard");
 
 module.exports = institute => {
   const msg = `Отлично, теперь давай определим твоё направление. Выбери его из предложенных ниже.`;
-  const keyboard = createKeyboard(
-    [
-      "ИФСТ",
-      "ПИНФ",
-      "ПИНЖ",
-      "ИВЧТ",
-      "ИФБС",
-      "ИБС",
-      "КИЛП",
-      "ДИЗН",
-      "РКЛМ",
-      "ТЛВД"
-    ],
-    4,
-    "group"
-  );
+  switch (institute) {
+    case 1:
+      arr = [
+        "ИФСТ",
+        "ПИНФ",
+        "ПИНЖ",
+        "ИВЧТ",
+        "ИФБС",
+        "ИБС",
+        "КИЛП",
+        "ДИЗН",
+        "РКЛМ",
+        "ТЛВД"
+      ];
+      break;
+    case 2:
+      arr = [];
+      break;
+    case 3:
+      break;
+    case 4:
+      break;
+    case 5:
+      break;
+    case 6:
+      break;
+    default:
+      arr = [];
+  }
+  const keyboard = createKeyboard(arr, 4, "group");
   return [msg, keyboard, null];
 };
