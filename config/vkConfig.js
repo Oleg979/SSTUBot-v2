ee201d743713e16733b0539b471a1bf7311536aa
Олег <olegsolovev506@gmail.com>
2018-08-19 10:27:40 +0400

    Changed token

--- config/vkConfig.js
+++ config/vkConfig.js
@@ -2,7 +2,7 @@ module.exports = {
   authToken:
     "84ffd08d22f099a9c7e0a83c82fd48f1e4338d705783bfdf03f723fcb09170f1daf56ce8eb7bb12b7e951",
   groupToken:
-    "8a0f6c7f90d0810067a2765fb9cfdf4e12290409f789f10d605bb64f1f24a31b1f19da6af1df97df936ec",
+    "fe6b550e9962e57c5f5df33c1b740efa9306304b48ebc43c36a6296eecff058447ded5d97400ad711b34e",
   appID: 6347837,
   appSecret: "2Y0USubDFTvjdCet3lLV",
   mode: "oauth",
