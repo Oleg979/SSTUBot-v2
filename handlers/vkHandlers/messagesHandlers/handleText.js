ad755509c8da4045387d2d1b93af4a9d0e024e66
Олег <olegsolovev506@gmail.com>
2018-08-19 19:34:55 +0400

    add

--- handlers/vkHandlers/messagesHandlers/handleText.js
+++ handlers/vkHandlers/messagesHandlers/handleText.js
@@ -2,7 +2,7 @@ var User = require("../dbHandlers/userSchema");
 var defineInstitute = require("./messagesHandlers/registration/defineInstitute");
 
 module.exports = async (id, text) => {
-  const count = await User.countDocuments({ id: from_id });
+  const count = await User.countDocuments({ id: id });
   if (count > 0)
     return [
       "Для общения со мной используй кнопки меню. Чтобы отобразить меню, нажми на значок справа от поля ввода.",
