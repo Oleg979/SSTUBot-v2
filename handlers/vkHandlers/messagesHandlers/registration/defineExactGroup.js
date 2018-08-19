2b47fc6fb502218c24fdc19b097f0b4b8db81637
Олег <olegsolovev506@gmail.com>
2018-08-19 17:19:52 +0400

    as

--- handlers/vkHandlers/messagesHandlers/registration/defineExactGroup.js
+++ handlers/vkHandlers/messagesHandlers/registration/defineExactGroup.js
@@ -9,11 +9,15 @@ var options = {
 
 module.exports = async group => {
   const $ = await rp(options);
-  const groups = $(".col-group a").map((i, el) =>
-    $(el)
-      .text()
-      .get()
-  );
+  const groups = $(".col-group a")
+    .map((i, el) =>
+      $(el)
+        .text()
+        .get()
+    )
+    .filter((i, el) => {
+      return $(el).indexOf(group) >= 0;
+    });
   console.log("GROUPS: ", groups);
   //var keyboard = createKeyboard(groups, 4, "groupName");
   var msg = "Вот какие группы мне удалось найти. Выбери среди них свою.";
