e6ff4b11a1ee0a76b5e253eafe6f7a0584237345
Олег <olegsolovev506@gmail.com>
2018-08-19 20:13:03 +0400

    add

---
+++ handlers/vkHandlers/messagesHandlers/actions/schedule.js
@@ -0,0 +1,14 @@
+var cheerio = require("cheerio");
+var rp = require("request-promise");
+var User = require("../../../dbHandlers/userSchema");
+
+module.exports = async id => {
+  var user = await User.findOne({ id });
+  console.log(user);
+  var group = user.group;
+  var options = {
+    uri: `http://rasp.sstu.ru${group}`,
+    transform: body => cheerio.load(body)
+  };
+  const $ = await rp(options);
+};
