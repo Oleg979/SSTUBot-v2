5f51a1c227889588442246bc1335bf527c9eac87
Олег <olegsolovev506@gmail.com>
2018-09-04 20:19:23 +0400

    add

--- handlers/vkHandlers/others/userData.js
+++ handlers/vkHandlers/others/userData.js
@@ -1,5 +1,5 @@
 var axios = require("axios");
-var { appID, appSecret, authToken, mode } = require("../../../config/vkConfig");
+var { authToken } = require("../../../config/vkConfig");
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
@@ -7,6 +7,7 @@ const getUserDataById = async id => {
   const res = await axios.get(
     `https://api.vk.com/method/users.get?user_ids=${id}&access_token=${authToken}&v=5.80`
   );
+  console.log(res.data);
   return res.data.response[0];
 };
 
