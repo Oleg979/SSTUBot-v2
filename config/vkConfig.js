4fa032fd6f447e64ef059c84aaa631697991a820
Олег <olegsolovev506@gmail.com>
2018-09-04 20:25:39 +0400

    add

--- config/vkConfig.js
+++ config/vkConfig.js
@@ -1,10 +1,10 @@
 module.exports = {
   authToken:
-    "84ffd08d22f099a9c7e0a83c82fd48f1e4338d705783bfdf03f723fcb09170f1daf56ce8eb7bb12b7e951",
+    "32b14d89172834771d5f3a2fddaf8db7d4986d974f76ddddd64e97e2f4df49c98cc05d624a89d5982e088",
   groupToken:
     "fe6b550e9962e57c5f5df33c1b740efa9306304b48ebc43c36a6296eecff058447ded5d97400ad711b34e",
-  appID: 6347837,
-  appSecret: "2Y0USubDFTvjdCet3lLV",
+  appID: 6682568,
+  appSecret: "FOJgdtKuW24fyaufnqsw",
   mode: "oauth",
   confirmToken: "c982a697",
   groupId: "170258511"
