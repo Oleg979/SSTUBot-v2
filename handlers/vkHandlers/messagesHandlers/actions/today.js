6bd230511168a5ab79ed464534735ad049d42fca
Олег <olegsolovev506@gmail.com>
2018-09-04 17:30:22 +0400

    add

--- handlers/vkHandlers/messagesHandlers/actions/today.js
+++ handlers/vkHandlers/messagesHandlers/actions/today.js
@@ -39,31 +39,12 @@ module.exports = async id => {
       var children = $(el)
         .children()
         .find(".rasp-table-inner-cell")
-        .children();
-      var aud = children.find(".aud").text();
-      if (aud == "") {
-        res.push(`${i + 1}. -`);
-        return;
-      }
-      var subject = children.find(".subject").text();
-      var type = children.find(".type").text();
-      var teacher = children.find(".teacher").text();
-      num++;
-
-      var subjectm = children.find(".subject-m").text();
-      if (subjectm != "") {
-        var sub = children.find(".subgroup");
-        res.push(
-          `${i + 1}. ${subjectm} ${sub.eq(0).text()} ${sub.eq(1).text()}`
-        );
-      } else
-        res.push(
-          `${i +
-            1}. ${subject} ${type} в ${aud} ауд. у преподавателя ${teacher}`
-        );
+        .children()
+        .text();
+      res.push(`${children}\n`);
     }
   });
-  return [num, null, null];
+  return [res, null, null];
   /*  row.each((i, el) => {
     $(el)
       .children()
