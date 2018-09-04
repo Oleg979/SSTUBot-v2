177660c1bf3c953b397c1183ae6327c96359268a
Олег <olegsolovev506@gmail.com>
2018-09-04 17:54:04 +0400

    add

--- handlers/vkHandlers/messagesHandlers/actions/today.js
+++ handlers/vkHandlers/messagesHandlers/actions/today.js
@@ -61,37 +61,5 @@ module.exports = async id => {
         );
     }
   });
-  console.log(res.join("\n"));
   return [res.join("\n"), null, null];
-  /*  row.each((i, el) => {
-    $(el)
-      .children()
-      .find(".rasp-table-inner-cell")
-      .each((idx, e) => {
-        var children = $(e).children();
-        var aud = children.find(".aud").text();
-        if (aud == "") {
-          res.push(`${i + 1}. -`);
-          return;
-        }
-        var subject = children.find(".subject").text();
-        var type = children.find(".type").text();
-        var teacher = children.find(".teacher").text();
-        num++;
-
-        var subjectm = children.find(".subject-m").text();
-        if (subjectm != "") {
-          var sub = children.find(".subgroup");
-          res.push(
-            `${i + 1}. ${subjectm} ${sub.eq(0).text()} ${sub.eq(1).text()}`
-          );
-        } else
-          res.push(
-            `${i +
-              1}. ${subject} ${type} в ${aud} ауд. у преподавателя ${teacher}`
-          );
-      });
-  });
-
-  return [res, num]; */
 };
