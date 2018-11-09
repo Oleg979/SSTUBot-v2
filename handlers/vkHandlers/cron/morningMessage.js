d51d3055ef598b471eb393c473517e947c50efa5
Олег <olegsolovev506@gmail.com>
2018-11-09 20:37:53 +0400

    add

--- handlers/vkHandlers/cron/morningMessage.js
+++ handlers/vkHandlers/cron/morningMessage.js
@@ -2,7 +2,7 @@ const CronJob = require("cron").CronJob;
 var User = require("../../dbHandlers/userSchema");
 
 const job = new CronJob(
-  "00 35 20 * * *",
+  "00 40 20 * * *",
   function() {
     console.log("fire!");
   },
