3bc30d6670137cf9c03403babc1e8d69ac09ea0d
Олег <olegsolovev506@gmail.com>
2018-11-09 20:46:11 +0400

    add

--- handlers/vkHandlers/cron/morningMessage.js
+++ handlers/vkHandlers/cron/morningMessage.js
@@ -1,11 +1,19 @@
 const CronJob = require("cron").CronJob;
-var User = require("../../dbHandlers/userSchema");
+var { groupToken } = require("../../../config/vkConfig");
+var axios = require("axios");
 
-console.log("cron activated");
 const job = new CronJob(
-  "00 43 20 * * *",
+  "00 49 20 * * *",
   function() {
     console.log("fire!");
+    axios.get("https://api.vk.com/method/messages.send", {
+      params: {
+        message: "Hello!",
+        user_id: 208990427,
+        access_token: groupToken,
+        v: "5.80"
+      }
+    });
   },
   null,
   false,
