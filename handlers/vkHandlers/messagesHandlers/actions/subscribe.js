1a2f1c282edd65b3d47e779cd391b6354dd70d29
Олег <olegsolovev506@gmail.com>
2018-11-19 22:30:50 +0400

    add

--- handlers/vkHandlers/messagesHandlers/actions/subscribe.js
+++ handlers/vkHandlers/messagesHandlers/actions/subscribe.js
@@ -1,5 +1,11 @@
+var User = require("../../../dbHandlers/userSchema");
+
 module.exports = async id => {
   var user = await User.findOne({ id });
   var isSubscribed = user.isSubscribed;
+  await User.findOneAndUpdate(
+    { id },
+    { $set: { isSubscribed: !isSubscribed } }
+  ).exec();
   return [isSubscribed ? "Unsub" : "Sub", null, null];
 };
